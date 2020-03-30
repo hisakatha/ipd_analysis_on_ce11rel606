386d988cf148c0bbd2686baf96bcba07318c4e46
hisakatha <hisakatha@users.noreply.github.com>
2020-03-31 07:46:20 +0900

    Add scripts for analysing feature enrichment using only C. elegans data

--- jun2018_analysis/enrichment_inside_features/plot.functions.R
+++ jun2018_analysis/enrichment_inside_features/plot.functions.R
@@ -49,6 +49,24 @@ plot.enrichment.dual_facet.all_sample <- function(data, ylab_text) {
         scale_y_continuous(expand = expand_scale(mult = c(0.1, 0.2)))
 }
 
+plot.enrichment.dual_facet.all_sample.v2 <- function(data, ylab_text) {
+    sub_data <- data
+    #sub_data[region != "All", label := .(sprintf("%s\np = %.3g", region, pvalue))]
+    #sub_data[region == "All", label := .(sprintf("%s\n(Control)", region))]
+    sub_data[, qvalue := .(p.adjust(pvalue, method = "BH"))][, qvalue_mark := .(ifelse(qvalue > 0.05, "", ifelse(qvalue > 0.01, "*", ifelse(qvalue > 0.001, "**", "***"))))]
+    sub_data$base <- factor(sub_data$base, levels = c("A", "C", "G", "T", "ALL"))
+    sub_data[, "sample" := ifelse(sample == "k_normBy_ab", "VC2010+OP50", ifelse(sample == "l_normBy_cd", "VC2010",
+        ifelse(sample == "ab", "VC2010+OP50/WGA", ifelse(sample == "cd", "VC2010/WGA",
+        ifelse(sample == "k", "VC2010+OP50/native", ifelse(sample == "l", "VC2010/native",
+        ifelse(sample == "PD2182", "PD2182 (PacBio RS II)", ifelse(sample == "PD2182sequel", "PD2182 (PacBio Sequel v1.2)", sample))))))))]
+    sub_data[, "sample" := factor(sample, levels = c("VC2010+OP50/WGA", "VC2010/WGA", "VC2010+OP50/native", "VC2010/native", "PD2182 (PacBio RS II)", "PD2182 (PacBio Sequel v1.2)", "VC2010+OP50", "VC2010"))]
+    ggplot(sub_data, aes(sample, enrichment)) + geom_point() + facet_grid(base ~ region_label) +
+        geom_text(aes(label = qvalue_mark), vjust = 0, hjust = 0.5, size = 8 / ggplot2::.pt) +
+        xlab("Sample") + ylab(ylab_text) + geom_hline(yintercept = 1, alpha = 0.5) +
+        theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5), plot.title = element_text(hjust = 0.5)) +
+        scale_y_continuous(expand = expand_scale(mult = c(0.1, 0.2)))
+}
+
 # For WGA samples
 plot.enrichment.dual_facet.all_sample.subset1 <- function(data, ylab_text) {
     sub_data <- data
@@ -101,6 +119,69 @@ plot.enrichment.dual_facet.all_sample.subset3 <- function(data, ylab_text) {
     sub_data <- sub_data[region == "promoter" | region == "exon" | region == "intron" | region == "three prime utr" | region == "TTS [0, 500]" | region == "tandem repeat"]
     ggplot(sub_data, aes(sample, enrichment)) + geom_point() + facet_grid(base ~ region_label) +
         geom_text(aes(label = qvalue_mark), vjust = 0, hjust = 0.5) +
+        xlab("Sample") + ylab(ylab_text) + geom_hline(yintercept = 1, alpha = 0.5) +
+        theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5), plot.title = element_text(hjust = 0.5)) +
+        scale_y_continuous(expand = expand_scale(mult = c(0.1, 0.2)))
+}
+
+# For WGA samples
+plot.enrichment.dual_facet.all_sample.subset1.v2 <- function(data, ylab_text) {
+    sub_data <- data
+    #sub_data[region != "All", label := .(sprintf("%s\np = %.3g", region, pvalue))]
+    #sub_data[region == "All", label := .(sprintf("%s\n(Control)", region))]
+    sub_data[, qvalue := .(p.adjust(pvalue, method = "BH"))][, qvalue_mark := .(ifelse(qvalue > 0.05, "", ifelse(qvalue > 0.01, "*", ifelse(qvalue > 0.001, "**", "***"))))]
+    sub_data$base <- factor(sub_data$base, levels = c("A", "C", "G", "T", "ALL"))
+    sub_data <- sub_data[sample == "ab" | sample == "cd" | sample == "k" | sample == "l"]
+    sub_data[, "sample" := ifelse(sample == "k_normBy_ab", "VC2010+OP50", ifelse(sample == "l_normBy_cd", "VC2010",
+        ifelse(sample == "ab", "VC2010+OP50/WGA", ifelse(sample == "cd", "VC2010/WGA",
+        ifelse(sample == "k", "VC2010+OP50/native", ifelse(sample == "l", "VC2010/native",
+        ifelse(sample == "PD2182", "PD2182 (PacBio RS II)", ifelse(sample == "PD2182sequel", "PD2182 (PacBio Sequel v1.2)", sample))))))))]
+    sub_data[, "sample" := factor(sample, levels = c("VC2010+OP50/WGA", "VC2010/WGA", "VC2010+OP50/native", "VC2010/native", "PD2182 (PacBio RS II)", "PD2182 (PacBio Sequel v1.2)", "VC2010+OP50", "VC2010"))]
+    sub_data <- sub_data[region == "enhancer" | region == "five prime utr" | region == "exon" | region == "intron" | region == "three prime utr" | region == "tandem repeat"]
+    ggplot(sub_data, aes(sample, enrichment)) + geom_point() + facet_grid(base ~ region_label) +
+        geom_text(aes(label = qvalue_mark), vjust = 0, hjust = 0.5, size = 8 / ggplot2::.pt) +
+        xlab("Sample") + ylab(ylab_text) + geom_hline(yintercept = 1, alpha = 0.5) +
+        theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5), plot.title = element_text(hjust = 0.5)) +
+        scale_y_continuous(expand = expand_scale(mult = c(0.1, 0.2)))
+}
+
+# For WGA samples
+plot.enrichment.dual_facet.all_sample.subset2.v2 <- function(data, ylab_text) {
+    sub_data <- data
+    #sub_data[region != "All", label := .(sprintf("%s\np = %.3g", region, pvalue))]
+    #sub_data[region == "All", label := .(sprintf("%s\n(Control)", region))]
+    sub_data[, qvalue := .(p.adjust(pvalue, method = "BH"))][, qvalue_mark := .(ifelse(qvalue > 0.05, "", ifelse(qvalue > 0.01, "*", ifelse(qvalue > 0.001, "**", "***"))))]
+    sub_data$base <- factor(sub_data$base, levels = c("A", "C", "G", "T", "ALL"))
+    sub_data <- sub_data[sample == "ab" | sample == "cd" | sample == "k" | sample == "l"]
+    sub_data[, "sample" := ifelse(sample == "k_normBy_ab", "VC2010+OP50", ifelse(sample == "l_normBy_cd", "VC2010",
+        ifelse(sample == "ab", "VC2010+OP50/WGA", ifelse(sample == "cd", "VC2010/WGA",
+        ifelse(sample == "k", "VC2010+OP50/native", ifelse(sample == "l", "VC2010/native",
+        ifelse(sample == "PD2182", "PD2182 (PacBio RS II)", ifelse(sample == "PD2182sequel", "PD2182 (PacBio Sequel v1.2)", sample))))))))]
+    sub_data[, "sample" := factor(sample, levels = c("VC2010+OP50/WGA", "VC2010/WGA", "VC2010+OP50/native", "VC2010/native", "PD2182 (PacBio RS II)", "PD2182 (PacBio Sequel v1.2)", "VC2010+OP50", "VC2010"))]
+    sub_data <- sub_data[region == "enhancer" | region == "promoter" | region == "five prime utr" | region == "exon" | region == "intron" | region == "three prime utr" | region == "tandem repeat"]
+    ggplot(sub_data, aes(sample, enrichment)) + geom_point() + facet_grid(base ~ region_label) +
+        geom_text(aes(label = qvalue_mark), vjust = 0, hjust = 0.5, size = 8 / ggplot2::.pt) +
+        xlab("Sample") + ylab(ylab_text) + geom_hline(yintercept = 1, alpha = 0.5) +
+        theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5), plot.title = element_text(hjust = 0.5)) +
+        scale_y_continuous(expand = expand_scale(mult = c(0.1, 0.2)))
+}
+
+# For native samples
+plot.enrichment.dual_facet.all_sample.subset3.v2 <- function(data, ylab_text) {
+    sub_data <- data
+    #sub_data[region != "All", label := .(sprintf("%s\np = %.3g", region, pvalue))]
+    #sub_data[region == "All", label := .(sprintf("%s\n(Control)", region))]
+    sub_data[, qvalue := .(p.adjust(pvalue, method = "BH"))][, qvalue_mark := .(ifelse(qvalue > 0.05, "", ifelse(qvalue > 0.01, "*", ifelse(qvalue > 0.001, "**", "***"))))]
+    sub_data$base <- factor(sub_data$base, levels = c("A", "C", "G", "T", "ALL"))
+    sub_data <- sub_data[sample == "k_normBy_ab" | sample == "l_normBy_cd"]
+    sub_data[, "sample" := ifelse(sample == "k_normBy_ab", "VC2010+OP50", ifelse(sample == "l_normBy_cd", "VC2010",
+        ifelse(sample == "ab", "VC2010+OP50/WGA", ifelse(sample == "cd", "VC2010/WGA",
+        ifelse(sample == "k", "VC2010+OP50/native", ifelse(sample == "l", "VC2010/native",
+        ifelse(sample == "PD2182", "PD2182 (PacBio RS II)", ifelse(sample == "PD2182sequel", "PD2182 (PacBio Sequel v1.2)", sample))))))))]
+    sub_data[, "sample" := factor(sample, levels = c("VC2010+OP50/WGA", "VC2010/WGA", "VC2010+OP50/native", "VC2010/native", "PD2182 (PacBio RS II)", "PD2182 (PacBio Sequel v1.2)", "VC2010+OP50", "VC2010"))]
+    sub_data <- sub_data[region == "promoter" | region == "exon" | region == "intron" | region == "three prime utr" | region == "TTS [0, 500]" | region == "tandem repeat"]
+    ggplot(sub_data, aes(sample, enrichment)) + geom_point() + facet_grid(base ~ region_label) +
+        geom_text(aes(label = qvalue_mark), vjust = 0, hjust = 0.5, size = 8 / ggplot2::.pt) +
         xlab("Sample") + ylab(ylab_text) + geom_hline(yintercept = 1, alpha = 0.5) +
         theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5), plot.title = element_text(hjust = 0.5)) +
         scale_y_continuous(expand = expand_scale(mult = c(0.1, 0.2)))
